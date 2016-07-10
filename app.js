00c669dc89956b012fa8f785d5723e61538a72f3
Fernando Rangel <ferangelfe@gmail.com>
2016-07-10 10:04:00 -0300

    Update - Add Google Maps Location
    Add Google Maps Javascript API

--- app.js
+++ app.js
@@ -30,6 +30,45 @@ app.controller('myCtrl', function($scope, myResource) {
 
 		});
     }
+
+    var geocoder;
+	var map;
+	var marker;
+	initialize();
+	function initialize() {
+	    var latlng = new google.maps.LatLng(-18.8800397, -47.05878999999999);
+	    var options = {
+	        zoom: 5,
+	        center: latlng,
+	        mapTypeId: google.maps.MapTypeId.ROADMAP
+	    };
+	 
+	    map = new google.maps.Map(document.getElementById("mapa"), options);
+	 
+	    geocoder = new google.maps.Geocoder();
+	 
+	    marker = new google.maps.Marker({
+	        map: map,
+	        draggable: true,
+	    });
+	 
+	    marker.setPosition(latlng);
+	}
+	$scope.carregarNoMapa = function(endereco) {
+	    geocoder.geocode({ 'address': endereco + ', Brasil', 'region': 'BR' }, function (results, status) {
+	        if (status == google.maps.GeocoderStatus.OK) {
+	            if (results[0]) {
+	                var latitude = results[0].geometry.location.lat();
+	                var longitude = results[0].geometry.location.lng();
+	                var location = new google.maps.LatLng(latitude, longitude);
+	                
+	                marker.setPosition(location);
+	                map.setCenter(location);
+	                map.setZoom(16);
+	            }
+	        }
+	    });
+	}
 });
 
 app.factory('myResource', function ($resource) {
